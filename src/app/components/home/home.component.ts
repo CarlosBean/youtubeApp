68dbc3496e9855cc704d6eea1cecdb0e5b1d41c6
Carlos Andres Benavides <cabenavidesd@misena.edu.co>
2019-01-13 23:01:17 -0500

    fix: solved selected video loading

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,14 +11,25 @@ declare var $: any;
 export class HomeComponent implements OnInit {
 
   videos: any;
+  channel: any;
   selectedVideo: any;
+  loading = true;
 
   constructor(private youtube: YoutubeService) { }
 
   ngOnInit() {
+    this.youtube.getChannel().subscribe(data => {
+      console.log(data);
+      this.channel = data;
+    });
+
     this.youtube.getVideos().subscribe(data => {
       this.videos = data;
     });
+  }
+
+  loaded() {
+    this.loading = false;
   }
 
   openModal(video) {
@@ -27,6 +38,7 @@ export class HomeComponent implements OnInit {
   }
 
   closeModal() {
+    this.loading = true;
     this.selectedVideo = null;
     $('#modelId').modal('hide');
   }
